825e119bf348d487ff9fd3b77ef3e6f8d2b36667
aunghtetpaing2021 <aunghtetpaing.dev@gmail.com>
2022-08-19 22:23:44 +0630

    [UPDATE] - add deskera model

--- app/Http/Controllers/DeskeraController.php
+++ app/Http/Controllers/DeskeraController.php
@@ -3,42 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Http\Client\Pool;
+use App\Models\DeskeraModel;
 
 class DeskeraController extends Controller
 {
-    protected $url;
-    protected $retry;
-    protected $timeout;
+    protected $deskera;
 
-    public $token;
-
-    public function __construct() {
-        $this->url = env('DESKERA_TOKEN_URL');
-        $this->retry = env('HTTP_RETRY');
-        $this->timeout = env('HTTP_TIMEOUT');
+    public function __construct(DeskeraModel $deskera) {
+        $this->deskera = $deskera;
     }
 
-    public function generateToken() {
-
-        // $responses = Http::pool(fn (Pool $pool) => [
-        //     $pool->get('http://localhost/first'),
-        //     $pool->get('http://localhost/second'),
-        //     $pool->get('http://localhost/third'),
-        // ]);
-
-        try {
-            $response = Http::retry($this->retry, $this->timeout)->get($this->url);
-            $status = $response->status();
-            
-            if($response->status() === 200) {
-                $data = json_decode($response->body(), true);
-                $this->token = $data['token'];
-            }
-
-        } catch(Expection $e) {
-            return Log::info($e);
-        }
+    public function index() {
+        $data = $this->deskera->generateToken();
+        return response()->json([
+            "data" => $data
+        ], 200);
     }
 }
