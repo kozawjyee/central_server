8a193a3876a2dea91b4b4ec50211ca75141b6b1a
kozawjyee <kozawgyi551997@gmail.com>
2022-09-20 11:26:52 +0630

    [customer api]

--- app/Models/DSK_Customer.php
+++ app/Models/DSK_Customer.php
@@ -23,68 +23,80 @@ class DSK_Customer extends Model
         $client = new Client([
             'base_uri' => $this->deskera->account_url
         ]);
+
+        $request = [
+            "username" => "admin",
+            "companyid" => "60bfa037-9865-4d1f-9bea-b6241b38701c",
+            "userid" => "9a7deb4e-a273-4a5b-b579-7b3691007552",
+            "accname" => "Trade Debtors",
+            "debitType" => true,
+            "sequenceformat" => "402880ce552a14ec01552a368718000d",
+            "currencyid" => "6",
+            "mappingcusaccid" => "402880505a026a07015a03993f5403d6",
+            "termid" => "ff8080814d4cdde1014d50ea331d0142",
+            "from" => "402880ce552a14ec01552a3684518000d",
+            "customername" => "Moe Thu Zar",
+            "isdefaultHeaderMap" => true,
+            "userfullname" => "admin",
+            "cdomain" => "shwetechinternal",
+            "customercode" => "C1",
+            "accountcode" => "Trade Debtors",
+            "acccode" => "CID000013",
+            "termvalue" => "NET10",
+            "creationDate" => "Aug 01, 2017 12:00:00 AM",
+            "currencycode" => "MMK",
+            "isVendor" => false,
+            "addressDetail" => 
+            [
+                [
+                "aliasNameID" => "Billing Address1",
+                "aliasName" => "Billing Address1",
+                "address" => "KUDALWADI, CHIKHALI,",
+                "county" => "",
+                "city" => "Pune City",
+                "state" => "Maharashtra",
+                "stateCode" => "",
+                "country" => "India",
+                "postalCode" => "412114",
+                "phone" => "",
+                "mobileNumber" => "9822070458 MR.NAIK",
+                "fax" => "",
+                "emailID" => "",
+                "recipientName" => "",
+                "contactPerson" => "",
+                "contactPersonNumber" => "",
+                "contactPersonDesignation" => "",
+                "website" => "",
+                "isBillingAddress" => true,
+                "isDefaultAddress" => true
+                ],
+                [
+                "aliasNameID" => "Shipping Address1",
+                "aliasName" => "Shipping Address1",
+                "address" => "KUDALWADI, CHIKHALI,",
+                "county" => "",
+                "city" => "",
+                "state" => "",
+                "stateCode" => "",
+                "country" => "",
+                "postalCode" => "",
+                "phone" => "",
+                "mobileNumber" => "",
+                "fax" => "",
+                "emailID" => "",
+                "recipientName" => "",
+                "contactPerson" => "",
+                "contactPersonNumber" => "",
+                "contactPersonDesignation" => "",
+                "website" => "",
+                "shippingRoute" => "",
+                "isBillingAddress" => false,
+                "isDefaultAddress" => true
+                ]
+            ]
+        ];
         
-        $response = $client->request('POST', '/rest/v1/master/customer?token='.$this->deskera->token.'&request={
-            "username": "admin",
-            "customername": "3P Enterprises",
-            "isdefaultHeaderMap": true,
-            "userfullname": "admin",
-            "cdomain": "rahulerptest47",
-            "customercode": "C1",
-            "accountcode": "Trade Debtors",
-            "termvalue": "NET10",
-            "creationDate": "Aug 01, 2017 12:00:00 AM",
-            "currencycode": "SGD",
-            "isVendor": false,
-            "addressDetail": [
-            {
-            "aliasNameID": "Billing Address1",
-            "aliasName": "Billing Address1",
-            "address": "KUDALWADI, CHIKHALI,",
-            "county": "myanmar",
-            "city": "Pune City",
-            "state": "Maharashtra",
-            "stateCode": "",
-            "country": "India",}
-            ]
-            "postalCode": "412114",
-            "phone": "09441887154",
-            "mobileNumber": "9822070458 MR.NAIK",
-            "fax": "0988565",
-            "emailID": "admin@g.com",
-            "recipientName": "mgmg",
-            "contactPerson": "ag ag",
-            "contactPersonNumber": "095541258",
-            "contactPersonDesignation": "abc",
-            "website": "www.djid.com",
-            "isBillingAddress": true,
-            "isDefaultAddress": true
-            },
-            {
-            "aliasNameID": "Shipping Address1",
-            "aliasName": "Shipping Address1",
-            "address": "KUDALWADI, CHIKHALI,",
-            "county": "yangon",
-            "city": "yangon",
-            "state": "yangon",
-            "stateCode": "0212",
-            "country": "myanmar",
-            "postalCode": "01101",
-            "phone": "0966562510",
-            "mobileNumber": "0955447412",
-            "fax": "09652451",
-            "emailID": "admin@g.com",
-            "recipientName": "mono",
-            "contactPerson": "mono",
-            "contactPersonNumber": "admin",
-            "contactPersonDesignation": "sdaf",
-            "website": "jfklasd",
-            "shippingRoute": "asdfsd",
-            "isBillingAddress": false,
-            "isDefaultAddress": true
-            }
-            ]
-            }');
+        $response = $client->request('POST', '/rest/v1/master/customer?token='.$this->deskera->token.'&request='.json_encode($request));
                 $json = $response->getBody()->getContents();
                 return Log::channel('deskera')->info($json);
 
